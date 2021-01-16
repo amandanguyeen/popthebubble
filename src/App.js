989823e4c9b97236745e9659d6f9c6c3727788e0
Amanda Nguyen <amanda6@ualberta.ca>
2021-01-16 15:28:36 -0700

    created header, no image so far

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import React, { Component } from "react";
+import { Header, MainContent } from './components/common';
 
 
 class App extends Component {
     render(){
         return (
             <div class="App">
+                <Header />
+                <MainContent />
             </div>
         );
     }
---
+++ src/components/common/maincontent/MainContent.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class MainContent extends Component {
+    render() {
+        return(
+            <section className = "mainContent">
+                HII
+            </section>
+        );
+    }
+}
+
+export default MainContent;
