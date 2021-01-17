e900ee339cdb7f69b62fb5363665aae843d1a48f
Amanda Nguyen <56659167+amandanguyeen@users.noreply.github.com>
2021-01-17 04:13:52 -0700

    Revert "created front end section of create"

--- api/routes/testAPI.js
+++ api/routes/testAPI.js
@@ -6,45 +6,15 @@ var express = require("express");
 var router = express.Router();
 
 router.get("/", function (req, res, next) {
-  res.send("API is working properly");
-});
-
-router.get("/GET/", function (req, res, next) {
-  const titles = new Array();
-  const comments = new Array();
-  const newComments = new Array();
-  const mainContent = new Map();
-
-  const titles = db.all("SELECT TITLE FROM ARTICLES LIMIT 4", (error, rows) => {
+  db.all("SELECT ID, TITLE FROM ARTICLES", (error, rows) => {
+    console.log(rows);
     rows.forEach((row) => {
-      titles.push(row.TITLE);
+      console.log(row.ID + " " + row.TITLE);
     });
   });
-
-  console.log(titles);
-
-  titles.forEach((title) => {
-    db.all(
-      `SELECT COMMENT FROM COMMENTS WHERE TITLE = ? LIMIT 2`,
-      title,
-      (error, rows) => {
-        rows.forEach((row) => {
-          comments.push(row.COMMENT);
-        });
-      }
-    );
-  });
-
-  titles.forEach((title) => {
-    newComments.push(comments.pop());
-    newComments.push(comments.pop());
-    console.log(newComments);
-    mainContent.set(title, newComments);
-    newComments.pop();
-    newComments.pop();
-  });
-
-  res.send(mainContent);
+  console.log(db);
+  console.log("HELLO???");
+  res.send("API is working properly");
 });
 
 module.exports = router;
