c0807bfbebe5b622fa6e9a3f40fa68629a90a250
Amanda Nguyen <amanda6@ualberta.ca>
2021-01-17 04:03:15 -0700

    created front end section of create

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,8 +1,7 @@
-import Header from './header';
-import Navbar from './navbar';
-import MainContent from './maincontent';
+import Header from "./header";
+import Navbar from "./navbar";
+import MainContent from "./maincontent";
+import PagesHeader from "./pagesheader";
+import PagesNavbar from "./pagesnavbar";
 
-export {
-    Header, Navbar, MainContent
-    
-}
+export { Header, Navbar, MainContent, PagesHeader, PagesNavbar };
