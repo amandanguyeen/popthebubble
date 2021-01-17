18b49140afe946f5f9d910e2c00d9a182ebbab86
Amanda Nguyen <amanda6@ualberta.ca>
2021-01-17 00:00:58 -0700

    SUCCESSFULLY added sqlitedb thanks to Monica Bui and Chelsea Hong

--- src/components/common/maincontent/MainContent.jsx
+++ src/components/common/maincontent/MainContent.jsx
@@ -1,64 +1,66 @@
 import React, { Component } from "react";
-import './MainContent.css'
+import "./MainContent.css";
 
 class MainContent extends Component {
-    render() {
-        return(
-            <section className = "mainContent">
-                <LeftSection />
-                <TitleSection />
-                <RightSection />
-            </section>
-        );
-    }
+  render() {
+    return (
+      <section className="mainContent">
+        <LeftSection />
+        <TitleSection />
+        <RightSection />
+      </section>
+    );
+  }
 }
 
 export default MainContent;
 
-
-class LeftSection extends Component{
-    render(){
-        return(
-            <section 
-                className = "left-section" 
-                style={{height: 180, width: 360, overflow: 'scroll'}}
-            >
-                <ol>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-                 </ol>
-            </section>
-        );
-    }
+class LeftSection extends Component {
+  render() {
+    return (
+      <section
+        className="left-section"
+        style={{ height: 180, width: 360, overflow: "scroll" }}
+      >
+        <ol>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco
+        </ol>
+      </section>
+    );
+  }
 }
 
-class TitleSection extends Component{
-    render(){
-        return(
-            <section 
-                className = "title-section" 
-                style={{height: 190, width: 525, overflow: 'scroll'}}
-            >
-                <ol>
-                Joe Biden should proceed to launch an investigation on questionable events during the election. 
-                 </ol>
-            </section>
-        );
-    }
+class TitleSection extends Component {
+  render() {
+    return (
+      <section
+        className="title-section"
+        style={{ height: 190, width: 525, overflow: "scroll" }}
+      >
+        <ol>
+          Joe Biden should proceed to launch an investigation on questionable
+          events during the election.
+        </ol>
+      </section>
+    );
+  }
 }
 
-
-
-class RightSection extends Component{
-    render(){
-        return(
-            <section 
-                className = "right-section" 
-                style={{height: 180, width: 360, overflow: 'scroll'}}
-            >
-                <ol>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco 
-                 </ol>
-            </section>
-        );
-    }
+class RightSection extends Component {
+  render() {
+    return (
+      <section
+        className="right-section"
+        style={{ height: 180, width: 360, overflow: "scroll" }}
+      >
+        <ol>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco
+        </ol>
+      </section>
+    );
+  }
 }
